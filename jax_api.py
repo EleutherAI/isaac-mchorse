c71f33439cb742ec61f9e4f67b64ca843c22caee
Ben Wang <wangben3@gmail.com>
2021-05-07 19:46:44 +1000

    Update jax_api.py

--- jax_api.py
+++ jax_api.py
@@ -5,7 +5,7 @@ import json
 
 def jax_complete(text, top_p=0.9, temp=0.75):
     resp = requests.post(headers={"content-type": "application/json"},
-                         url="http://34.90.220.168:5000/complete",
+                         url="https://vm.eleuther.ai/complete",
                          data=json.dumps({"context": text,
                                           "top_p": top_p,
                                           "temp": temp}))
