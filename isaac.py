64a97f0617d381105d856a6789fa74153ea9329f
Louis Castricato <wirezapp@gmail.com>
2021-06-25 23:34:28 -0400

    Update isaac.py
    random goose mode

--- isaac.py
+++ isaac.py
@@ -53,6 +53,18 @@ async def on_message(message):
           f.write(r.content)
         picture = discord.File("goose.jpg")
         await message.channel.send(file=picture)
+        
+    #by louis
+    if random.random() < 0.0001:
+        idx = random.randint(1, 1000)
+        file = f"goose-mugshot-{str(idx).zfill(4)}.jpg"
+        url = f"https://raw.githubusercontent.com/steggie3/goose-dataset/master/images/{file}"
+        r = requests.get(url)
+        with open("goose.jpg", "wb") as f:
+          f.write(r.content)
+        picture = discord.File("goose.jpg")
+        await message.channel.send(file=picture)
+
 
     if str(message.channel) != 'the-faraday-cage':
         return
